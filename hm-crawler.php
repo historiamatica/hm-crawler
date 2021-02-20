f08c865f9e5725da320a5a3dfa3e1a6326d213ab
Joshua L.J. Vachon <joshua.vachon@historiamati.ca>
2021-02-20 01:30:14 -0500

    Crawling récursif

--- hm-crawler.php
+++ hm-crawler.php
@@ -1,24 +1,52 @@
 <?php
     include("class/Tentacule.php");
 
-    function creationLien($from, $to){
+    $crawled = array();
+    $crawling = array();
 
+    function creationLien($href, $url){
+        $protocole = parse_url($url)["scheme"];
+        $domaine = parse_url($url)["host"];
+
+        if(substr($href, 0, 2) == "//"){
+            $href = $protocole . ":" . $href;
+        }else if(substr($href, 0 ,1)=="/"){
+            $href = $protocole . "://" . $domaine . $href;
+        }
+
+        return $href;
     }
 
     function bougerTentacule($url){
+        global $crawled;
+        global $crawling;
+
         $hmParser = new Tentacule($url);
         $listeURL = $hmParser -> obtenirLiens();
-            foreach ($listeURL as $lien){
-                $href = $lien->getAttribute("href");
+        foreach ($listeURL as $lien){
+            $href = $lien->getAttribute("href");
 
-                if(strpos($href, "#") !== false){
-                    continue;
-                }else if(substr($href, 0, 11) == "javascript:"){
-                    continue;
-                }
-
-                echo $href."<br>";
+            if(strpos($href, "#") !== false){
+                continue;
+            }else if(substr($href, 0, 11) == "javascript:"){
+                continue;
             }
+
+            $href = creationLien($href, $url);
+
+            if(!in_array($href, $crawled)){
+                $crawled[] = $href;
+                $crawling[] = $href;
+            }
+
+
+            echo $href."<br>";
+        }
+        array_shift($crawling);
+        foreach($crawling as $site){
+            bougerTentacule($site);
+        }
+
         }
 
 
