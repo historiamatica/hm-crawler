82318cd7d0d702d874a0d7522031b76201710650
Joshua L.J. Vachon <joshua.vachon@historiamati.ca>
2021-03-03 15:53:24 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
+    <title>Humatica - L'histoire à l'Ère du numérique</title>
+    <meta name="description" content="Humatica est un portail pour les humanités numériques et pour la démocratisation du savoir dans les sciences humaines et sociales."> 
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Historiamatica</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link rel="stylesheet" href="css/app.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script> 
 </head>
 <body>
-    <div id="app">
-        
-    </div>
+    
 </body>
 </html>
