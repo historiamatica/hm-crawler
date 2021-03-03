84a41b875675e83e27b862e4571f98fedd474e7d
Joshua L.J. Vachon <joshua.vachon@historiamati.ca>
2021-03-03 16:33:39 -0500

    Update index.php

--- index.php
+++ index.php
@@ -11,30 +11,11 @@
     <link rel="stylesheet" href="css/app.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script> 
 </head>
-<body>
-<nav> <!-- navbar content here  --> </nav>
-
-  <ul id="slide-in" class="sidenav">
-    <li><div class="user-view">
-      <div class="background">
-        <img src="images/office.jpg">
-      </div>
-      <a href="#user"><img class="circle" src="images/yuna.jpg"></a>
-      <a href="#name"><span class="white-text name">John Doe</span></a>
-      <a href="#email"><span class="white-text email">jdandturk@gmail.com</span></a>
-    </div></li>
-    <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
-    <li><a href="#!">Second Link</a></li>
-    <li><div class="divider"></div></li>
-    <li><a class="subheader">Subheader</a></li>
-    <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
-  </ul>
-
-  <script>
-       document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-  });
-  </script>
+<body class="grey lighten-4">
+  <nav>
+    <div class="nav-wrapper grey lighten-5">
+      Test
+    </div>
+  </nav>
 </body>
 </html>
