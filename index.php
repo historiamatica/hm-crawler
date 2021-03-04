128dfcc4f85bbb72aa4562928168ca8aa8d1de06
Joshua L.J. Vachon <joshua.vachon@historiamati.ca>
2021-03-04 09:06:04 -0500

    Framework CSS

--- index.php
+++ index.php
@@ -6,16 +6,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <link rel="stylesheet" href="css/app.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script> 
+    <link rel="stylesheet" href="hm-framework/0.1/css/hm.css">
+
 </head>
-<body class="grey lighten-4">
-  <nav>
-    <div class="nav-wrapper grey lighten-5">
-      Test
-    </div>
-  </nav>
+<body>
+  
+  <script src="hm-framework/0.1/js/hm.js"></script>
 </body>
 </html>
