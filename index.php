0016391b617cbf09edf05eb9781773bf54fbc935
Joshua L.J. Vachon <joshua.vachon@historiamati.ca>
2021-03-04 17:38:57 -0500

    HM CSS Framework Update

--- index.php
+++ index.php
@@ -6,11 +6,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="hm-framework/0.1/css/hm.css">
+    <link rel="stylesheet" href="hm-framework/0.1/css/style.css">
 
 </head>
 <body>
-  
+  <h1>Lorem ipsum</h1>
+  <h2>Lorem ipsum</h2>
+  <h3>Lorem ipsum</h3>
+  <h4>Lorem ipsum</h4>
+  <h5>Lorem ipsum</h5>
+  <h6>Lorem ipsum</h6>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic sed magni eaque officiis adipisci at ducimus maiores, necessitatibus accusantium iusto, harum similique laboriosam ipsum ab? Sapiente tempore mollitia error rerum!</p>
+  <blockquote>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur ad possimus dolore rem laboriosam id ea illum quas consectetur dolor. Expedita, aliquid similique eligendi provident sunt excepturi iste labore quis?</blockquote>
+  <a href="#">Test <div class="badge primary">5 nouveaux!</div></a><br>
+  <i class="i64">add_circle</i>
   <script src="hm-framework/0.1/js/hm.js"></script>
+  
 </body>
 </html>
