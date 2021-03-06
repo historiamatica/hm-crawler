de39a1c61fa3ca2f68ddf55d25c1f9f654c9824f
Joshua L.J. Vachon <joshua.vachon@historiamati.ca>
2021-03-06 10:08:11 -0500

    Nouveau design

--- index.php
+++ index.php
@@ -1,17 +1,33 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <title>Humatica - L'histoire à l'Ère du numérique</title>
-    <meta name="description" content="Humatica est un portail pour les humanités numériques et pour la démocratisation du savoir dans les sciences humaines et sociales."> 
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="res/app.css">
+	<title>Humatica - L'histoire à l'Ère du numérique</title>
+	<meta name="description" content="Humatica est un portail pour les humanités numériques et pour la démocratisation du savoir dans les sciences humaines et sociales."> 
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="res/app.css">
 
 </head>
 <body>
- 
- 
-  <script src="res/app.js"></script>
+	<nav class="sideNav">
+
+	</nav>
+	<main>
+		<nav class="topNav">
+			<a href="#">Réalisations</a>
+			<a href="#">Services</a>
+			<a href="#">Confidentialité</a>
+			<a href="#">À propos</a>
+		</nav>
+		<section class="moduleRecherche">
+			<form action="recherche.php" method="post">
+				<input id="barreRecherche" type="text" placeholder="Recherche">
+				<button id="btnRecherche"><i class="i32">search</i></button>
+			</form>
+		</section>
+	</main>
+
+	<script src="res/app.js"></script>
 </body>
 </html>
